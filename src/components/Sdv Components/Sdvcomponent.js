44ea999659fba2df7cd8cbe9ab19a0250263f72e
swarajjaiswal <swaraj93310@gmail.com>
2025-01-08 03:42:47 +0530

    final changes

--- src/components/Sdv Components/Sdvcomponent.js
+++ src/components/Sdv Components/Sdvcomponent.js
@@ -31,13 +31,11 @@ const cardHeadings = [
   "Chassis",
   "Bluetooth Module HC-05",
   "Battery Management System",
-
   "Arduino UNO",
   "Servo Motors",
   "L298 Motor Controller",
   "BO Motors",
   "IR Proximity Sensor",
-
   "Cells",
   "Ultrasonic Sensor",
   "Switches",
@@ -54,16 +52,13 @@ const cardHeadings = [
 
 const cardParagraphs = [
   "The chassis is the frame of the robot, which holds all the components together and provides structural support.",
-
   "The HC-05 is a Bluetooth module that allows devices to communicate wirelessly over a short distance. It is commonly used in robotics and IoT projects to enable wireless communication between devices.",
   "A Battery Management System (BMS) is an electronic system that ensures the safe operation of a rechargeable battery by monitoring its state, protecting it, controlling its environment, and balancing it",
- 
   "The Arduino Uno is an open-source microcontroller board based on the Microchip ATmega328P microcontroller (MCU) and developed by Arduino.cc and initially released in 2010.",
   "A servo motor is defined as an electric motor that allows for precise control of angular or linear position, speed, and torque.",
   "L298N module is a high voltage, high current dual full-bridge motor driver module for controlling DC motor and stepper motor. It can control both the speed and rotation direction of two DC motors.",
   "BO Motor, stands for Battery Operated Motor, is a simple DC Geared Motor consists of gearbox and shaft, used in Robot, cars and other DIY projects where movement is needed.",
   "The IR sensor or infrared sensor is one kind of electronic component, used to detect specific characteristics in its surroundings through emitting or detecting IR radiation.",
-
   "Electric cells are devices that convert chemical energy into electrical energy through a process involving oxidation and reduction.",
   "Ultrasonic sensors are electronic devices that calculate the target’s distance by emission of ultrasonic sound waves and convert those waves into electrical signals.",
   "A Switch is a device which is designed to interrupt the current flow in a circuit. In simple words, a Switch can make or break an electrical circuit.",
